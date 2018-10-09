d91fd72775e99870073afd4ddc8277dadcf25273
Balur <abhin.balur@hpe.com>
2018-10-09 19:31:39 +0530

    all files

---
+++ src/main/java/io/jenkins/plugins/TestExampleBuildAction.java
@@ -0,0 +1,44 @@
+package io.jenkins.plugins;
+
+import hudson.model.AbstractBuild;
+import hudson.model.Action;
+
+
+public class TestExampleBuildAction implements Action {
+
+    private String message;
+    private AbstractBuild<?, ?> build;
+
+    @Override
+    public String getIconFileName() {
+        return "/plugin/testExample/img/build-goals.png";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Test Example Build Page";
+    }
+
+    @Override
+    public String getUrlName() {
+        return "testExampleBA";
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public int getBuildNumber() {
+        return this.build.number;
+    }
+
+    public AbstractBuild<?, ?> getBuild() {
+        return build;
+    }
+
+    TestExampleBuildAction(final String message, final AbstractBuild<?, ?> build)
+    {
+        this.message = message;
+        this.build = build;
+    }
+}
